3c74759ba2dbd4c1ad27b428b145a83dbbe4d4a0
Tavish07 <84187988+Tavish07@users.noreply.github.com>
2021-05-19 18:38:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(800,400);
+ movingRect= createSprite(400, 200, 50, 50);
+ fixedRect= createSprite(400, 200, 50, 50);
+ fixedRect.shapeColor="red"
+ movingRect.shapeColor="red"
+}
+
+function draw() {
+  background(255,255,255); 
+  movingRect.x=World.mouseX 
+  movingRect.y=World.mouseY
+  if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2
+    &fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2
+    &movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2
+    &fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2)
+    {
+    movingRect.shapeColor="green"
+    fixedRect.shapeColor="green"
+    
+  }
+  else{
+    movingRect.shapeColor="red"
+    fixedRect.shapeColor="red"
+  }
+  drawSprites();
+
+}
